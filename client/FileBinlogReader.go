651728f201b4a6ea0aca0b7355845130623d85f7
lancewang <lancewang@xishiqu.com>
2016-10-18 14:35:09 +0800

    fixed bug: index文件中纪录的是绝对路径时的路径处理错误

--- client/FileBinlogReader.go
+++ client/FileBinlogReader.go
@@ -75,7 +75,9 @@ func (this *FileBinlogReader) Dump(position uint32, filename string) error {
 		if line, _, err := reader.ReadLine(); nil != err {
 			break
 		} else {
-			logFile := strings.Split(string(line), "/")[1]
+			lastSepPos := strings.LastIndex(string(line), "/")
+			logFile := string(line[lastSepPos+1:])
+			fmt.Println(logFile)
 			if logFile == filename {
 				this.index = index
 			}
